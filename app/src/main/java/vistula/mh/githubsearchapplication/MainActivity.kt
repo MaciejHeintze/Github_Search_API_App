e508c2127c4c516ab6dab123203a73b0620122ac
Maciek <maciej.heintze96@gmail.com>
2020-12-08 20:51:24 +0100

    migrating first screen to fragment (navigation component) commit

--- app/src/main/java/vistula/mh/githubsearchapplication/MainActivity.kt
+++ app/src/main/java/vistula/mh/githubsearchapplication/MainActivity.kt
@@ -1,102 +1,23 @@
 package vistula.mh.githubsearchapplication
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.OrientationHelper
-import kotlinx.android.synthetic.main.activity_main.*
-import vistula.mh.githubsearchapplication.model.GithubModel
-import vistula.mh.githubsearchapplication.retrofit.MainViewModel
-import vistula.mh.githubsearchapplication.retrofit.MainViewModelFactory
-import vistula.mh.githubsearchapplication.retrofit.Repository
-import vistula.mh.githubsearchapplication.retrofit.RepositoryAdapter
+import vistula.mh.githubsearchapplication.ui.SearchRepositoryFragment
 
 const val TAG = "RETROFIT_API_CALL"
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var repositoryNameSearchQuery: String
-    private lateinit var viewModel: MainViewModel
-    private var dataList: MutableList<GithubModel> = mutableListOf()
-    private lateinit var repositoryAdapter: RepositoryAdapter
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setupRecyclerView()
-        getInputRepositoryName()
-    }
+        setContentView(R.layout.repository_search_layout)
 
-    private fun getInputRepositoryName(){
-        search_edit_text_id.addTextChangedListener(object : TextWatcher {
-
-            var handler: Handler = Handler(Looper.getMainLooper())
-            var workRunnable: Runnable? = null
-
-            override fun afterTextChanged(mEdit: Editable) {
-                if(mEdit.length > 3) {
-                    workRunnable = Runnable { fetchDataByRepositoryName(mEdit.toString()) }
-                    handler.postDelayed(workRunnable!!, 1000)
-                }
-            }
-            override fun beforeTextChanged(
-                s: CharSequence,
-                start: Int,
-                count: Int,
-                after: Int
-            ) {
-            }
-            override fun onTextChanged(
-                s: CharSequence,
-                start: Int,
-                before: Int,
-                count: Int
-            ) {
-            }
-        })
-    }
-
-    private fun fetchDataByRepositoryName(mEdit: String){
-        if(dataList.isNotEmpty()){
-            dataList.clear()
-            repositoryAdapter.notifyDataSetChanged()
+        if (savedInstanceState == null) {
+            val fragment = SearchRepositoryFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.main_content, fragment)
+                .commit()
         }
-        repositoryNameSearchQuery = mEdit
-        if(!repositoryNameSearchQuery.isNullOrEmpty()){
-            fetchData(repositoryNameSearchQuery)
-        }else{
-            dataList.clear()
-            repositoryAdapter.notifyDataSetChanged()
-        }
-    }
-
-    private fun setupRecyclerView(){
-        repositoryAdapter= RepositoryAdapter(dataList)
-        recycler_view_id.layoutManager= LinearLayoutManager(this)
-        recycler_view_id.addItemDecoration(DividerItemDecoration(this, OrientationHelper.VERTICAL))
-        recycler_view_id.adapter=repositoryAdapter
-    }
-
-    private fun fetchData(repoName: String){
-        val repository = Repository()
-        val viewModelFactory = MainViewModelFactory(repository)
-        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
-        viewModel.getRepositoryByName(repoName)
-        viewModel.myResponse.observe(this, Observer { response ->
-            if(response.isSuccessful){
-                response.body()?.let { dataList.add(it) }
-                repositoryAdapter.notifyDataSetChanged()
-            }else{
-                Log.d(TAG, response.message())
-            }
-        })
     }
 }
---
+++ app/src/main/java/vistula/mh/githubsearchapplication/ui/SearchRepositoryFragment.kt
@@ -0,0 +1,112 @@
+package vistula.mh.githubsearchapplication.ui
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.OrientationHelper
+import kotlinx.android.synthetic.main.repository_search_layout.*
+import vistula.mh.githubsearchapplication.R
+import vistula.mh.githubsearchapplication.TAG
+import vistula.mh.githubsearchapplication.model.GithubModel
+import vistula.mh.githubsearchapplication.retrofit.MainViewModel
+import vistula.mh.githubsearchapplication.retrofit.MainViewModelFactory
+import vistula.mh.githubsearchapplication.retrofit.Repository
+import vistula.mh.githubsearchapplication.retrofit.RepositoryAdapter
+
+class SearchRepositoryFragment : Fragment(){
+
+    private lateinit var repositoryNameSearchQuery: String
+    private lateinit var viewModel: MainViewModel
+    private var dataList: MutableList<GithubModel> = mutableListOf()
+    private lateinit var repositoryAdapter: RepositoryAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.repository_search_layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        getInputRepositoryName()
+    }
+
+    private fun setupRecyclerView(){
+        repositoryAdapter= RepositoryAdapter(dataList)
+        recycler_view_id.layoutManager= LinearLayoutManager(context)
+        recycler_view_id.addItemDecoration(DividerItemDecoration(context, OrientationHelper.VERTICAL))
+        recycler_view_id.adapter=repositoryAdapter
+    }
+
+    private fun getInputRepositoryName(){
+        search_edit_text_id.addTextChangedListener(object : TextWatcher {
+
+            var handler: Handler = Handler(Looper.getMainLooper())
+            var workRunnable: Runnable? = null
+
+            override fun afterTextChanged(mEdit: Editable) {
+                if(mEdit.length > 3) {
+                    workRunnable = Runnable { fetchDataByRepositoryName(mEdit.toString()) }
+                    handler.postDelayed(workRunnable!!, 1000)
+                }
+            }
+            override fun beforeTextChanged(
+                s: CharSequence,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+            override fun onTextChanged(
+                s: CharSequence,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+            }
+        })
+    }
+
+
+    private fun fetchDataByRepositoryName(mEdit: String){
+        if(dataList.isNotEmpty()){
+            dataList.clear()
+            repositoryAdapter.notifyDataSetChanged()
+        }
+        repositoryNameSearchQuery = mEdit
+        if(!repositoryNameSearchQuery.isNullOrEmpty()){
+            fetchData(repositoryNameSearchQuery)
+        }else{
+            dataList.clear()
+            repositoryAdapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun fetchData(repoName: String){
+        val repository = Repository()
+        val viewModelFactory = MainViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+        viewModel.getRepositoryByName(repoName)
+        viewModel.myResponse.observe(this, Observer { response ->
+            if(response.isSuccessful){
+                response.body()?.let { dataList.add(it) }
+                repositoryAdapter.notifyDataSetChanged()
+            }else{
+                Log.d(TAG, response.message())
+            }
+        })
+    }
+}
