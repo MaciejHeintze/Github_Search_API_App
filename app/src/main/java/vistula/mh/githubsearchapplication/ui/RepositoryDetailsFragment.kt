b1f388aaed075e1f66d972e39f1b327275b991e3
Maciek <maciej.heintze96@gmail.com>
2020-12-08 22:29:36 +0100

    repository details layout implementation commit

---
+++ app/src/main/java/vistula/mh/githubsearchapplication/ui/RepositoryDetailsFragment.kt
@@ -0,0 +1,18 @@
+package vistula.mh.githubsearchapplication.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import vistula.mh.githubsearchapplication.R
+
+class RepositoryDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_repository_details, container, false)
+    }
+}
