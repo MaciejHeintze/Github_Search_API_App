97f5fa0a2eba8d999812e279726410bfaef7caec
Maciek <maciej.heintze96@gmail.com>
2020-12-10 13:11:10 +0100

    on back button pressed in details fragment commit

--- app/src/main/java/vistula/mh/githubsearchapplication/ui/RepositoryDetailsFragment.kt
+++ app/src/main/java/vistula/mh/githubsearchapplication/ui/RepositoryDetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_repository_details.view.*
 import vistula.mh.githubsearchapplication.R
@@ -20,7 +21,14 @@ class RepositoryDetailsFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_repository_details, container, false)
         initValues(view)
+        onBackButtonPressed(view)
         return view
+    }
+
+    private fun onBackButtonPressed(view: View){
+        view.back_button_id.setOnClickListener {
+            findNavController(this).setGraph(R.navigation.repository_graph)
+        }
     }
 
     private fun initValues(view: View){
